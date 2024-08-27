d3b9aace8390b5316cd79b1b8e460bdc905ad038
Snozzyy <oliverschuller@hotmail.com>
2024-08-27 18:52:16 +0200

    Added accelerometer

--- app/src/main/java/com/example/mobilt_java23_oliver_schuller_shakev4/MainActivity.java
+++ app/src/main/java/com/example/mobilt_java23_oliver_schuller_shakev4/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.mobilt_java23_oliver_schuller_shakev4;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +15,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor accelerometerSensor;
-    private SensorEventListener accelerometerEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +29,36 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 
-            sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE)
+            Log.d("onCreate", "Initialize Sensor Service");
+
+            sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+            accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(MainActivity.this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+            Log.d("onCreate", "Registered accelerometer listener");
 
             return insets;
         });
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        ImageView imageViewBox = findViewById(R.id.imgViewBox);
+        Log.d("onSensorChanged", "X: " + sensorEvent.values[0] + " Y: " + sensorEvent.values[1] + " Z: " + sensorEvent.values[2]);
+
+        if (sensorEvent.values[0] > 25) {
+            Log.d("xAxis", "Left");
+            imageViewBox.setX(imageViewBox.getX() - 100);
+        } else if (sensorEvent.values[0] < -25) {
+            Log.d("xAxis", "Right");
+            imageViewBox.setX(imageViewBox.getX() + 100);
+        }
+
+
     }
 }
