43d267e71fde0a8dac659069f824d602d53a2b3c
Snozzyy <oliverschuller@hotmail.com>
2024-08-29 16:23:40 +0200

    Changed functionality of Y and X-axis

--- app/src/main/java/com/example/mobilt_java23_oliver_schuller_shakev4/MainActivity.java
+++ app/src/main/java/com/example/mobilt_java23_oliver_schuller_shakev4/MainActivity.java
@@ -8,6 +8,7 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +19,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
-    private Sensor accelerometerSensor;
+    private Sensor accelerometerSensor, gyroscopeSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +30,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 
-            Log.d("onCreate", "Initialize Sensor Service");
-
+            Log.d("onCreate", "Initialize Sensor Services");
             sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-            accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            sensorManager.registerListener(MainActivity.this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
-            Log.d("onCreate", "Registered accelerometer listener");
+            accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            if (accelerometerSensor != null) {
+                sensorManager.registerListener(MainActivity.this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                Log.d("onCreate", "Accelerometer initialized");
+            } else
+                Log.d("onCreate", "Accelerometer not available");
 
             return insets;
         });
@@ -49,22 +52,38 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         ImageView imageViewBox = findViewById(R.id.imgViewBox);
-        Log.d("onSensorChanged", "Y: " + sensorEvent.values[1] + " Z: " + sensorEvent.values[2]);
+        ProgressBar progressBarLeft = findViewById(R.id.progressLeft);
+        ProgressBar progressBarRight = findViewById(R.id.progressRight);
 
-        if (sensorEvent.values[0] > 20) {
-            Log.d("xAxis", "Left");
-            imageViewBox.setX(imageViewBox.getX() - 50);
-        } else if (sensorEvent.values[0] < -20) {
-            Log.d("xAxis", "Right");
-            imageViewBox.setX(imageViewBox.getX() + 50);
+
+        // Log accelerometer values
+        Log.d("AccelerometerValues", "Y: " + sensorEvent.values[0] + " Y: " + sensorEvent.values[1] + " Z: " + sensorEvent.values[2]);
+        Log.d("BoxScale", "X: " + imageViewBox.getScaleX() + " Y: " + imageViewBox.getScaleY());
+
+        // Scale the box based on the accelerometer y-axis value
+        if (sensorEvent.values[1] > 7 && imageViewBox.getScaleX() < 3) {
+            Log.d("yAxis", "Up");
+            imageViewBox.setScaleX((float) (imageViewBox.getScaleX() + 0.1));
+            imageViewBox.setScaleY((float) (imageViewBox.getScaleY() + 0.1));
+        } else if (sensorEvent.values[1] < 3 && imageViewBox.getScaleX() > 0.1) {
+            Log.d("yAxis", "Down");
+            imageViewBox.setScaleX((float) (imageViewBox.getScaleX() - 0.1));
+            imageViewBox.setScaleY((float) (imageViewBox.getScaleY() - 0.1));
         }
 
-        if (sensorEvent.values[1] > 10) {
-            Log.d("yAxis", "Down - Axis = " + sensorEvent.values[1]);
-            imageViewBox.setY(imageViewBox.getY() + 250);
-        } else if (sensorEvent.values[1] < -10) {
-            Log.d("yAxis", "Up - Axis = " + sensorEvent.values[1]);
-            imageViewBox.setY(imageViewBox.getY() - 50);
+        // Move the progress bar left or right depending on the accelerometer x-axis value
+        if (sensorEvent.values[0] > 5) {
+            Log.d("xAxis", "Left");
+            if (progressBarRight.getProgress() != 0)
+                progressBarRight.setProgress(progressBarRight.getProgress() - 5);
+            else
+                progressBarLeft.setProgress(progressBarLeft.getProgress() + 5);
+        } else if (sensorEvent.values[0] < -5){
+            Log.d("xAxis", "Right");
+            if (progressBarLeft.getProgress() != 0)
+                progressBarLeft.setProgress(progressBarLeft.getProgress() - 5);
+            else
+                progressBarRight.setProgress(progressBarRight.getProgress() + 5);
         }
     }
 }
