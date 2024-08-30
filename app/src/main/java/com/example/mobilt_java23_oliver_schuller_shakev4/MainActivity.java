19057f2696fef2ff921d930a94442a0891372810
Snozzyy <oliverschuller@hotmail.com>
2024-08-30 20:08:45 +0200

    Added functionality for z-axis

--- app/src/main/java/com/example/mobilt_java23_oliver_schuller_shakev4/MainActivity.java
+++ app/src/main/java/com/example/mobilt_java23_oliver_schuller_shakev4/MainActivity.java
@@ -1,34 +1,48 @@
 package com.example.mobilt_java23_oliver_schuller_shakev4;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor accelerometerSensor, gyroscopeSensor;
+    private float xAxis, yAxis, zAxis;
+    Switch bgSwitch;
+    private boolean isSwitchOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            View decorView = getWindow().getDecorView();
+            decorView.setBackgroundColor(Color.DKGRAY);
 
             Log.d("onCreate", "Initialize Sensor Services");
             sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -39,6 +53,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 Log.d("onCreate", "Accelerometer initialized");
             } else
                 Log.d("onCreate", "Accelerometer not available");
+
+            bgSwitch = findViewById(R.id.bgSwitch);
+
+            // Listen for changes in the switch and change the background color accordingly
+            bgSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                    if(isChecked) {
+                        decorView.setBackgroundColor(Color.RED);
+                        isSwitchOn = true;
+                    }
+                    else {
+                        decorView.setBackgroundColor(Color.DKGRAY);
+                        isSwitchOn = false;
+                    }
+                }
+            });
 
             return insets;
         });
@@ -54,36 +85,54 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         ImageView imageViewBox = findViewById(R.id.imgViewBox);
         ProgressBar progressBarLeft = findViewById(R.id.progressLeft);
         ProgressBar progressBarRight = findViewById(R.id.progressRight);
+        TextView xAxisText = findViewById(R.id.xAxisText);
+        TextView yAxisText = findViewById(R.id.yAxisText);
+        TextView zAxisText = findViewById(R.id.zAxisText);
 
+        xAxis = sensorEvent.values[0];
+        yAxis = sensorEvent.values[1];
+        zAxis = sensorEvent.values[2];
+
+        // Update the text views with the accelerometer values
+        xAxisText.setText("X: " + xAxis);
+        yAxisText.setText("Y: " + yAxis);
+        zAxisText.setText("Z: " + zAxis);
 
         // Log accelerometer values
-        Log.d("AccelerometerValues", "Y: " + sensorEvent.values[0] + " Y: " + sensorEvent.values[1] + " Z: " + sensorEvent.values[2]);
-        Log.d("BoxScale", "X: " + imageViewBox.getScaleX() + " Y: " + imageViewBox.getScaleY());
-
-        // Scale the box based on the accelerometer y-axis value
-        if (sensorEvent.values[1] > 7 && imageViewBox.getScaleX() < 3) {
-            Log.d("yAxis", "Up");
-            imageViewBox.setScaleX((float) (imageViewBox.getScaleX() + 0.1));
-            imageViewBox.setScaleY((float) (imageViewBox.getScaleY() + 0.1));
-        } else if (sensorEvent.values[1] < 3 && imageViewBox.getScaleX() > 0.1) {
-            Log.d("yAxis", "Down");
-            imageViewBox.setScaleX((float) (imageViewBox.getScaleX() - 0.1));
-            imageViewBox.setScaleY((float) (imageViewBox.getScaleY() - 0.1));
-        }
+        Log.d("AccelerometerValues", "Y: " + xAxis + " Y: " + yAxis + " Z: " + zAxis);
 
         // Move the progress bar left or right depending on the accelerometer x-axis value
-        if (sensorEvent.values[0] > 5) {
+        if (xAxis > 5) {
             Log.d("xAxis", "Left");
             if (progressBarRight.getProgress() != 0)
                 progressBarRight.setProgress(progressBarRight.getProgress() - 5);
             else
                 progressBarLeft.setProgress(progressBarLeft.getProgress() + 5);
-        } else if (sensorEvent.values[0] < -5){
+
+        } else if (xAxis < -5){
             Log.d("xAxis", "Right");
             if (progressBarLeft.getProgress() != 0)
                 progressBarLeft.setProgress(progressBarLeft.getProgress() - 5);
             else
                 progressBarRight.setProgress(progressBarRight.getProgress() + 5);
+        }
+
+        // Scale the box based on the accelerometer y-axis value
+        if (yAxis > 7 && imageViewBox.getScaleX() < 2.5) {
+            Log.d("yAxis", "Up");
+            imageViewBox.setScaleX((float) (imageViewBox.getScaleX() + 0.1));
+            imageViewBox.setScaleY((float) (imageViewBox.getScaleY() + 0.1));
+        } else if (yAxis < 3 && imageViewBox.getScaleX() > 0.1) {
+            Log.d("yAxis", "Down");
+            imageViewBox.setScaleX((float) (imageViewBox.getScaleX() - 0.1));
+            imageViewBox.setScaleY((float) (imageViewBox.getScaleY() - 0.1));
+        }
+
+        // Change the switch when meeting the z-value threshold
+        Log.d("zAxis", String.valueOf(zAxis));
+        if (zAxis > 38 || zAxis < -38) {
+            isSwitchOn = !isSwitchOn;
+            bgSwitch.setChecked(isSwitchOn);
         }
     }
 }
